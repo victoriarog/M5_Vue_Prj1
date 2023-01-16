3ec2b252b685fe06d8a6a86e45575d1a7a237bed
Vicka <vicrogatinskaya@gmail.com>
2023-01-16 11:46:03 +0100

    logins

--- prjTest/pages/index.vue
+++ prjTest/pages/index.vue
@@ -1,20 +1,31 @@
 <script setup>
-const {x ,y} = useMouseTracker();
-const counter = useState("counter", () => 0);
-function increment() {
-    counter.value++;
-}
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '~~/stores/user';
+
+// const {x ,y} = useMouseTracker();
+// const counter = useState("counter", () => 0);
+// function increment() {
+//     counter.value++;
+// }
+
+const userStore = useUserStore();
+const {isLogged, userName} = storeToRefs(userStore);
+
 </script>
 
 <template> 
-    <h1>
-        Mouse is at {{ x  }}, {{ y }}
+    <!-- <h1>
+        Mouse is at {{ x }}, {{ y }}
     </h1>
     <h1>
         Counter is {{ counter }}
-    </h1>
-    <button @click="increment">Increase counter</button>
+    </h1> -->
+    <!-- <button @click="increment">Increase counter</button> -->
 
-    <img src="~assets/img/chiCleanGif.gif"/>
+    <h2 v-if="isLogged">
+        hey, {{ userName }}
+    </h2>
+    <img v-if="isLogged" src="~assets/img/chiCleanGif.gif"/>
+
 
 </template>
